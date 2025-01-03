bd77b5964bdc52ba3dc2a36d036cef00f984a904
abhik345 <abhikdas345@gmail.com>
2025-01-03 12:31:33 +0530

    config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // This is supported and recommended for Next.js
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true, // This is supported and recommended for Next.js
+};
+
+export default nextConfig;
